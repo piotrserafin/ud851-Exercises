f21c5ca7e4beb63c369aadd8d51128b17128df1a
Piotr Serafin <piotr.serafin.71@gmail.com>
2017-11-24 14:44:53 +0100

    L6-T06.06 Completed

--- Lesson06-Visualizer-Preferences/T06.06-Exercise-AddTwoMoreCheckboxes/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.06-Exercise-AddTwoMoreCheckboxes/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -28,6 +28,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -48,15 +49,23 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
         setupPermissions();
     }
 
-    // TODO (4) Update the code in setupSharedPreferences and onSharedPreferenceChanged to update
+    // COMPLETED (4) Update the code in setupSharedPreferences and onSharedPreferenceChanged to
+    // update
     // mVisualizerView to show the current values stored in shared preferences.
     private void setupSharedPreferences() {
         // Get all of the values from shared preferences to set it up
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         mVisualizerView.setShowBass(sharedPreferences.getBoolean(getString(R.string.pref_show_bass_key),
                 getResources().getBoolean(R.bool.pref_show_bass_default)));
-        mVisualizerView.setShowMid(true);
-        mVisualizerView.setShowTreble(true);
+
+        mVisualizerView.setShowMid(sharedPreferences.getBoolean(getString(R.string
+                        .pref_show_mid_key), getResources().getBoolean(R.bool
+                .pref_show_mid_default)));
+
+        mVisualizerView.setShowTreble(sharedPreferences.getBoolean(getString(R.string
+                .pref_show_treble_key), getResources().getBoolean(R.bool.pref_show_treble_default)));
+
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
         // Register the listener
@@ -66,7 +75,21 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.pref_show_bass_key))) {
-            mVisualizerView.setShowBass(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_bass_default)));
+            mVisualizerView.setShowBass(sharedPreferences.getBoolean(key, getResources()
+                    .getBoolean(R.bool.pref_show_bass_default)));
+
+        }
+
+        if (key.equals(getString(R.string.pref_show_mid_key))) {
+            mVisualizerView.setShowMid(sharedPreferences.getBoolean(key, getResources()
+                    .getBoolean(R.bool.pref_show_mid_default)));
+
+        }
+
+        if (key.equals(getString(R.string.pref_show_treble_key))) {
+            mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key, getResources()
+                    .getBoolean(R.bool.pref_show_treble_default)));
+
         }
     }
 
