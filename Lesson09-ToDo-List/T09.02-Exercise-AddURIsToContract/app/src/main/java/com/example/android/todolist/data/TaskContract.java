3a2f435b0d6e0220d7b356c2dc1ea17926f4e316
Piotr Serafin <piotr.serafin.71@gmail.com>
2017-12-06 23:29:34 +0100

    L9-T09.03 Completed

--- Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -44,7 +44,7 @@ public class TaskContract {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                         .appendPath(PATH_TO_TASKS)
                         .build();
-        
+
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
 
